6922abb599cc7c6e6e02d781e13f8d590999df39
Lucas Felli <Lucas.felli@devinci.fr>
2018-03-16 18:01:58 +0100

    feat(populate): populate.js to create a json file with all the cars using node-car-api

---
+++ populate.js
@@ -0,0 +1,33 @@
+const {getBrands} = require('node-car-api');
+const {getModels} = require('node-car-api');
+const jsonfile = require('jsonfile');
+const file = 'car.json';
+
+async function populate() {
+  const brands = await getBrands();
+  
+  /*brands.forEach( async brand => {
+	  const models = await getModels(brand);
+	  models.forEach( model => {
+		  jsonfile.writeFile(file, model, {spaces: 2, flag: 'a'}, function(err) {
+			  if (err != null){
+				  console.error(err)
+			  }
+			})
+	  })
+  })*/
+  
+for(var i = 0; i < brands.length; i++) {	
+	var models = await getModels(brands[i]);
+	for(var j = 0; j < models.length; j++){
+			jsonfile.writeFile(file, models[j], {spaces: 2, flag: 'a'}, function(err) {
+			  if (err != null){
+				  console.error(err)
+			  }
+			})
+		}		
+	}
+}	
+
+
+populate();
